cf5a93e372f616f4a632cc7a6c3c7c824ee61c05
Will Crichton <wcrichto@cs.stanford.edu>
2017-04-10 01:59:40 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.system('ln -s ../build/libstorehouse.so storehousepy')
 
 setup(
     name='storehouse',
-    version='0.1.7',
+    version='0.1.8',
     url='https://github.com/scanner-research/storehouse',
     author='Alex Poms and Will Crichton',
     author_email='wcrichto@cs.stanford.edu',
