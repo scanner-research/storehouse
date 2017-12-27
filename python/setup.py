80194c7c2fb49461a6a53b7f2149062ed329c2a7
Will Crichton <wcrichto@cs.stanford.edu>
2017-12-27 14:07:31 -0500

    Make python build consistent with Scanner

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 import os
+import shutil
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-SO_PATH = os.path.abspath('{p:s}/../build/libstorehouse.so'.format(
-    p=SCRIPT_DIR))
-DEST_PATH = os.path.abspath('{p:s}/storehouse/'.format(p=SCRIPT_DIR))
-os.system('ln -s {from_path:s} {to_path:s}'.format(from_path=SO_PATH,
-                                                   to_path=DEST_PATH))
+STOREHOUSE_DIR = '.'
+BUILD_DIR = os.path.join(STOREHOUSE_DIR, 'build')
+PIP_DIR = os.path.join(BUILD_DIR, 'pip')
+SO_PATH = os.path.join(BUILD_DIR, 'libstorehouse.so')
+
+shutil.rmtree(PIP_DIR, ignore_errors=True)
+shutil.copytree(SCRIPT_DIR, PIP_DIR)
+shutil.copy(SO_PATH, os.path.join(PIP_DIR, 'storehouse'))
 
 setup(
     name='storehouse',
@@ -15,6 +19,7 @@ setup(
     author='Alex Poms and Will Crichton',
     author_email='wcrichto@cs.stanford.edu',
 
+    package_dir={'': PIP_DIR},
     packages=['storehouse'],
     package_data={
         'storehouse': [
